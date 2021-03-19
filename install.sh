14f3322d71093c3ac4ed77fab6126879e8b73e6a
IR170 <78010388+2Timeowl@users.noreply.github.com>
2021-03-20 01:40:10 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#installation
+
+clear
+
+gem install lolcat
+apt install toilet -y
+echo ""
+echo ""
+echo " VISIT MY WEBSITE 🔔 " |lolcat
+xdg-open https://linktr.ee/H4CKERsPUNK
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..." |lolcat
+sleep 1
+clear
+
+echo "Loading..." |lolcat
+sleep 1
+clear
+
+echo "Loading..." |lolcat
+sleep 1
+clear
+
+toilet -f future "HACKERSPUNK" |lolcat
+echo "-------------------------------------------------------" |lolcat
+echo   "Created  : PUNKER-BHAI $white" |lolcat
+echo   "Contact  : Instagram.com/hackerspunk $white" |lolcat
+echo "-------------------------------------------------------" |lolcat
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "HACKERSPUNK" |lolcat
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y
+pip install --upgrade pip
+pkg install nano php -y
+pip2 install mechanize
+pip2 install --upgrade pip
+
+echo ""
+echo "Coded By Punker-Bhai" |lolcat
+echo "________________________" |lolcat
+sleep 10
+echo ""
